0696f04faa04c23843aa9fe6425da8bea892082d
Maryna Isaienko <macprobp@gmail.com>
2024-05-23 20:54:44 -0700

    Initial Commit

---
+++ BankSystem/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  BankSystem
+//
+//  Created by Admin on 23/05/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
